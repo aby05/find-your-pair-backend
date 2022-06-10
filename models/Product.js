a4bbee9537d3bda61e30dc974bf2cd9bb0288009
aby05 <aberthel1401@gmail.com>
2022-06-10 11:06:18 +0400

    first commit

---
+++ models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Utils = require('../utils')
+
+// schema
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String    
+  },    
+  price: {
+    type: Number,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  image: {
+    type: String,
+    required: true    
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+
+
+}, { timestamps: true })
+
+
+// model
+const productModel = mongoose.model('Product', productSchema)
+
+// export
+module.exports = productModel
